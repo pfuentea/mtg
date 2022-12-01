6d410f385cc51175e63ceb55ecc7e2df073edf22
Patricio Fuentealba <pfuenteabl.deloitte@bancodechile.cl>
2022-12-01 11:12:20 -0300

    MOD: Se registran modelos a admin

--- ctm_app/admin.py
+++ ctm_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Edicion,Color,Tipo,Carta,Listados,ItemLista
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Edicion)
+admin.site.register(Color)
+admin.site.register(Tipo)
+admin.site.register(Carta)
+admin.site.register(Listados)
+admin.site.register(ItemLista)
